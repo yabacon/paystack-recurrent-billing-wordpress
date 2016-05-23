997e74c613f790449200581d4610b64af87700e9
ibrahimlawal <ibrahim@lawal.me>
2016-05-23 02:52:42 +0100

    Removed current_user_can check, bugfix on CSV export

--- links/export.php
+++ links/export.php
@@ -2,51 +2,45 @@
 
 require(dirname(__DIR__) . '/library/common.php');
 
-if (current_user_can( 'manage_options' )) {
-    // format = csv or json, default csv
-    $format = array_key_exists('json', $_GET) ? 'json' : 'csv';
-    // get a dump of data in subscribers table
-    $subscribers = paystack_recurrent_billing_get_all_subscribers();
-    
-    if($format==='json'){
-        header('Content-Type: application/json');
-        foreach($subscribers as $s){
-            $s->payments = json_decode($s->payments);
-        }
-        echo json_encode($subscribers);
+// format = csv or json, default csv
+$format = array_key_exists('json', $_GET) ? 'json' : 'csv';
+// get a dump of data in subscribers table
+$subscribers = paystack_recurrent_billing_get_all_subscribers();
+
+if($format==='json'){
+    header('Content-Type: application/json');
+    header('Content-disposition: filename="subscribers.json"');
+    foreach($subscribers as $s){
+        $s->payments = json_decode($s->payments);
     }
-    if($format==='csv'){
-        header('Content-Type: text/csv');
-        $str = '';
+    echo json_encode($subscribers);
+}
+if($format==='csv'){
+    header('Content-Type: text/csv');
+    header('Content-disposition: filename="subscribers.csv"');
+    $str = '';
+    $row = [];
+    $colnames_added = false;
+    foreach($subscribers as $s){
         $row = [];
-        // first row is col names
-        foreach($subscriber_info->cols as $c){
-            $row[] = '"' . str_replace('"', '\\"', $c->Field);
-        }
-        // add row
-        $str .= implode(',', $row);
-        $colnames_added = false;
-        foreach($subscribers as $s){
-            $row = [];
-            $vars = get_object_vars($s);
-            if(!$colnames_added){
-                $colnames = array_keys($vars);
-                // first row is col names
-                foreach($colnames as $c){
-                    $row[] = '"' . str_replace('"', "\\\"", $c);
-                }
-                // add row
-                $str .= implode(',', $row);
-                $row = [];
-                $colnames_added = true;
+        $vars = get_object_vars($s);
+        if(!$colnames_added){
+            $colnames = array_keys($vars);
+            // first row is col names
+            foreach($colnames as $c){
+                $row[] = '\'' . str_replace('\'', "\\".'\'', $c) . '\'';
             }
-            foreach($vars as $v){
-                $row[] = '"' . str_replace('"', "\\\"", $v);
-            }
+            // add row
             $str .= implode(',', $row);
             $row = [];
+            $colnames_added = true;
         }
-        echo $str;
+        foreach($vars as $v){
+            $row[] = '\'' . str_replace('\'', "\\".'\'', $v) . '\'';
+        }
+        $str .= "\n" . implode(',', $row);
+        $row = [];
     }
-
+    echo $str;
 }
+
